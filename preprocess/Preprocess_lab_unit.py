d6b7d8559c8a7698a782fecb18a0999742b7d30c
Tianchen Xu <tx2155@columbia.edu>
2019-05-29 04:28:36 -0400

    Clean units in lab_concept

--- preprocess/Preprocess_lab_unit.py
+++ preprocess/Preprocess_lab_unit.py
@@ -44,14 +44,17 @@ tmp['unit_concept_id'] = 8653.0
 tmp.loc[(tmp.value_as_number<122) & (tmp.value_as_number>86),'value_as_number'] = \
     (tmp.loc[(tmp.value_as_number<122) & (tmp.value_as_number>86),'value_as_number']-32)*5/9
 wrong_id = tmp.loc[~((tmp.value_as_number<50) & (tmp.value_as_number>30)),'measurement_id']
-lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
-lab_concept = to_cover(lab_concept, tmp)
+# lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
+# lab_concept = to_cover(lab_concept, tmp)
+tp1 = tmp.copy()
+w_id = wrong_id.copy()
 
 # Respiratory rate
 tmp=lab_concept.query('concept_name=="Respiratory rate"')[to_take]
 tmp.drop_duplicates(subset=['unit_concept_id','unit_source_value'])
 wrong_id = tmp.loc[tmp.value_as_number.isnull(),'measurement_id']
-lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
+# lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
+w_id = w_id.append(wrong_id)
 
 # BP systolic
 tmp=lab_concept.query('concept_name=="BP systolic"')[to_take]
@@ -59,8 +62,10 @@ tmp.drop_duplicates(subset=['unit_concept_id','unit_source_value'])
 concept_sub.query('concept_id==8876')
 tmp['unit_source_value'] = 'millimeter mercury column'
 wrong_id = tmp.loc[tmp.value_as_number.isnull(),'measurement_id']
-lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
-lab_concept = to_cover(lab_concept, tmp)
+# lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
+# lab_concept = to_cover(lab_concept, tmp)
+w_id = w_id.append(wrong_id)
+tp1 = tp1.append(tmp)
 
 # BP diastolic
 tmp=lab_concept.query('concept_name=="BP diastolic"')[to_take]
@@ -68,8 +73,10 @@ tmp.drop_duplicates(subset=['unit_concept_id','unit_source_value'])
 concept_sub.query('concept_id==8876')
 tmp['unit_source_value'] = 'millimeter mercury column'
 wrong_id = tmp.loc[tmp.value_as_number.isnull(),'measurement_id']
-lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
-lab_concept = to_cover(lab_concept, tmp)
+# lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
+# lab_concept = to_cover(lab_concept, tmp)
+w_id = w_id.append(wrong_id)
+tp1 = tp1.append(tmp)
 
 # Glucose [Mass/volume] in Blood | Glucose lab
 tmp=lab_concept.query('concept_name=="Glucose [Mass/volume] in Blood" | concept_name=="Glucose lab"')[to_take]
@@ -78,16 +85,20 @@ concept_sub.query('concept_id==8840.0')
 tmp['unit_source_value'] = 'mg/dL'
 tmp['unit_concept_id'] = 8840.0
 wrong_id = tmp.loc[tmp.value_as_number.isnull(),'measurement_id']
-lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
-lab_concept = to_cover(lab_concept, tmp)
+# lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
+# lab_concept = to_cover(lab_concept, tmp)
+w_id = w_id.append(wrong_id)
+tp1 = tp1.append(tmp)
 
 # eGFR with normals for black | eGFR with normals for non-black
 tmp=lab_concept.query('concept_name=="eGFR with normals for black" | concept_name=="eGFR with normals for non-black"')[to_take]
 tmp.drop_duplicates(subset=['unit_concept_id','unit_source_value'])
 tmp['unit_source_value'] = 'mL/min/1.73m2'
 wrong_id = tmp.loc[tmp.value_as_number.isnull(),'measurement_id']
-lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
-lab_concept = to_cover(lab_concept, tmp)
+# lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
+# lab_concept = to_cover(lab_concept, tmp)
+w_id = w_id.append(wrong_id)
+tp1 = tp1.append(tmp)
 
 # Body height Measured
 tmp=lab_concept.query('concept_name=="Body height Measured"')[to_take]
@@ -99,8 +110,10 @@ tmp['unit_concept_id'] = 8582.0
 tmp.loc[(tmp.value_as_number<80) & (tmp.value_as_number>25),'value_as_number'] = \
     tmp.loc[(tmp.value_as_number<80) & (tmp.value_as_number>25),'value_as_number']*2.54
 wrong_id = tmp.loc[~((tmp.value_as_number<203) & (tmp.value_as_number>63.5)),'measurement_id']
-lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
-lab_concept = to_cover(lab_concept, tmp)
+# lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
+# lab_concept = to_cover(lab_concept, tmp)
+w_id = w_id.append(wrong_id)
+tp1 = tp1.append(tmp)
 
 # Body weight
 tmp=lab_concept.query('concept_name=="Body weight"')[to_take]
@@ -116,8 +129,10 @@ tmp.loc[tmp.unit_source_value=='lb' ,'value_as_number'] = \
 wrong_id = tmp.loc[(tmp.value_as_number.isnull())|((tmp.unit_concept_id.isnull()) & tmp.unit_source_value.isnull()),'measurement_id']
 tmp['unit_source_value'] = 'kilogram'
 tmp['unit_concept_id'] = 9529.0
-lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
-lab_concept = to_cover(lab_concept, tmp)
+# lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
+# lab_concept = to_cover(lab_concept, tmp)
+w_id = w_id.append(wrong_id)
+tp1 = tp1.append(tmp)
 
 # Body mass index
 tmp=lab_concept.query('concept_name=="Body mass index"')[to_take]
@@ -126,8 +141,10 @@ concept_sub.query('concept_id==9531.0')
 wrong_id = tmp.loc[(tmp.value_as_number>70) | tmp.value_as_number.isnull(),'measurement_id']
 tmp['unit_source_value'] = 'kilogram per square meter'
 tmp['unit_concept_id'] = 9531.0
-lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
-lab_concept = to_cover(lab_concept, tmp)
+# lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
+# lab_concept = to_cover(lab_concept, tmp)
+w_id = w_id.append(wrong_id)
+tp1 = tp1.append(tmp)
 
 # Cholesterol [Mass/volume] in Serum or Plasma
 tmp=lab_concept.query('concept_name=="Cholesterol [Mass/volume] in Serum or Plasma"')[to_take]
@@ -136,8 +153,10 @@ concept_sub.query('concept_id==8840.0')
 tmp['unit_source_value'] = 'mg/dL'
 tmp['unit_concept_id'] = 8840.0
 wrong_id = tmp.loc[tmp.value_as_number.isnull(),'measurement_id']
-lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
-lab_concept = to_cover(lab_concept, tmp)
+# lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
+# lab_concept = to_cover(lab_concept, tmp)
+w_id = w_id.append(wrong_id)
+tp1 = tp1.append(tmp)
 
 # Cholesterol in HDL [Mass/volume] in Serum or Plasma
 tmp=lab_concept.query('concept_name=="Cholesterol in HDL [Mass/volume] in Serum or Plasma"')[to_take]
@@ -146,14 +165,20 @@ concept_sub.query('concept_id==8840.0')
 tmp['unit_source_value'] = 'mg/dL'
 tmp['unit_concept_id'] = 8840.0
 wrong_id = tmp.loc[tmp.value_as_number.isnull(),'measurement_id']
-lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
-lab_concept = to_cover(lab_concept, tmp)
+# lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
+# lab_concept = to_cover(lab_concept, tmp)
+w_id = w_id.append(wrong_id)
+tp1 = tp1.append(tmp)
 
 # Cholesterol in LDL [Mass/volume] in Serum or Plasma
 tmp=lab_concept.query('concept_name=="Cholesterol in LDL [Mass/volume] in Serum or Plasma"')[to_take]
 tmp.drop_duplicates(subset=['unit_concept_id','unit_source_value'])
 concept_sub.query('concept_id==8840.0')
 wrong_id = tmp.loc[tmp.value_as_number.isnull(),'measurement_id']
-lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
-lab_concept = to_cover(lab_concept, tmp)
+# lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(wrong_id)]
+# lab_concept = to_cover(lab_concept, tmp)
+w_id = w_id.append(wrong_id)
+tp1 = tp1.append(tmp)
 
+lab_concept = lab_concept.loc[~lab_concept.measurement_id.isin(w_id)]
+lab_concept = to_cover(lab_concept, tp1)
